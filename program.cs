20e138a324c54f890faed50bf219d7f69135d557
pratiksha korgaonkar <pratikshakorgaonkar05@gmail.com>
2024-10-10 15:41:44 +0530

    committing program.cs

---
+++ program.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        
+        Console.WriteLine($"Hello, {name}!");
+    }
+}
